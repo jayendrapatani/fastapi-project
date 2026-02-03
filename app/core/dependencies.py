668deb8a3cf89dc954a9a63b6fc1fc2e2762c292
jayendra <jayendrapatani9@gmail.com>
2026-02-03 09:42:48 +0530

    real auth

---
+++ app/core/dependencies.py
@@ -0,0 +1,17 @@
+from fastapi import Header,HTTPException
+from config import settings
+from security import verify_token
+
+
+def get_api_key(api_key:str=Header(...)):
+    if api_key != settings.API_KEY:
+        raise HTTPException(status_code=403,detail="INVALID API KEY")
+    
+
+
+
+def get_current_user(token:str=Header(...)):
+    payload=verify_token(token)
+    if not payload:
+        raise HTTPException(status_code=401,detail='Invalid JWT token')
+    return payload
