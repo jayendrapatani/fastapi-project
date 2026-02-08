e948396b1430d0d76f0060c75214141900124e7f
jayendra <jayendrapatani9@gmail.com>
2026-02-08 12:56:02 +0530

    deployment

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -1,18 +1,18 @@
-import json 
-import redis 
-from app.core.config import settings
+import os
+import redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_URL = os.getenv("REDIS_URL")
+
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 
-redis_client=redis.Redis.from_url(settings.REDIS_URL)
+def get_cached_prediction(key: str):
+    value = redis_client.get(key)
+    return eval(value) if value else None
 
 
-
-def get_sached_prediction(key:str):
-    value=redis_client.get(key)
-    if value:
-        return json.loads(value)
-    return None
-
-def set_cached_prediction(key:str,value:dir,expiry:int=3600):
-    redis_client.setex(key,expiry,json.dumps(value))
-    
+def set_cached_prediction(key: str, value: dict):
+    redis_client.set(key, str(value))
